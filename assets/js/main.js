c4ed083027b9c9ac0ee4f8d714d7a8a71f60bc6f
Joselin Grez <joselingrez.m@hotmail.es>
2017-05-19 10:33:24 -0300

    Ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+pictures.forEach(function(el, i){
+  var contenedor = document.getElementById("contenedor-imagen");
+  var divImagen = document.createElement("div");
+  divImagen.setAttribute("class","pandas");
+  var photo = document.createElement("img");
+  photo.src = pictures[i];
+  photo.id = 'a' + (i+1);
+
+  divImagen.appendChild(photo);
+  contenedor.appendChild(divImagen);
+});
